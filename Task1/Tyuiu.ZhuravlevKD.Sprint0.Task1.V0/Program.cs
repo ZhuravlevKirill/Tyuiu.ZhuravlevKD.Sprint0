b9f265502c37b825e3f236d645efa0914a7d1be2
ZhuravlevKirill <inciselmao@mail.ru>
2026-09-12 00:24:40 +0500

    Task1

---
+++ Task1/Tyuiu.ZhuravlevKD.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Tyuiu.ZhuravlevKD.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Кирилл";
+            int age = 19;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
